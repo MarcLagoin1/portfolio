c2b9ce07a819c55c188b6690fe74d398779bad33
Marc Lagoin <marclagoin@MacBook-Air-de-Marc.local>
2024-11-08 18:58:57 +0100

    ADD: expertise section

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,25 +1,95 @@
 import "../styles/Expertise.css";
+import { RiNextjsLine } from "react-icons/ri";
+import { BiLogoTypescript } from "react-icons/bi";
+import { FaPython, FaSwift, FaReact } from "react-icons/fa";
+import {
+  SiTensorflow,
+  SiKeras,
+  SiOpencv,
+  SiCsharp,
+  SiCplusplus,
+  SiOpengl,
+} from "react-icons/si";
+import { TbBrandThreejs } from "react-icons/tb";
 
 export const Expertise = () => {
   return (
     <div className="expertise">
-      <div className="card-container"></div>
-      <h1>Expertise</h1>
-      <div className="skill-grid">
-        <div className="skill">
-          <h3>Full Stack Web Development</h3>
-          <p>I have built ...</p>
-          <div className="techstack"></div>
-        </div>
-        <div className="skill">
-          <h3>Computer Vision</h3>
-          <p>I developed...</p>
-          <div className="techstack"></div>
-        </div>
-        <div className="skill">
-          <h3>Computer Graphics</h3>
-          <p>I used graphics...</p>
-          <div className="techstack"></div>
+      <div className="card-container">
+        <h1>My Expertise</h1>
+        <div className="skill-grid">
+          <div className="skill">
+            <h3>Full Stack Web Development</h3>
+            <p>Tech Stack:</p>
+            <div className="techstack">
+              <div className="tech-icon">
+                <BiLogoTypescript
+                  size={30}
+                  style={{ verticalAlign: "middle", color: "white" }}
+                />
+                <p>Typescript</p>
+              </div>
+              <div className="tech-icon">
+                <FaReact size={30} style={{ color: "white" }} />
+                <p>React</p>
+              </div>
+              <div className="tech-icon">
+                <RiNextjsLine size={30} style={{ color: "white" }} />
+                <p>NextJS</p>
+              </div>
+            </div>
+          </div>
+          <div className="skill">
+            <h3>Computer Vision</h3>
+            <p>Tech Stack:</p>
+            <div className="techstack">
+              <div className="tech-icon">
+                <FaPython size={30} style={{ color: "white" }} />
+                <p>Python</p>
+              </div>
+              <div className="tech-icon">
+                <SiCsharp size={30} style={{ color: "white" }} />
+                <p>C#</p>
+              </div>
+              <div className="tech-icon">
+                <SiTensorflow size={30} style={{ color: "white" }} />
+                <p>TensorFlow</p>
+              </div>
+              <div className="tech-icon">
+                <SiKeras size={30} style={{ color: "white" }} />
+                <p>Keras</p>
+              </div>
+              <div className="tech-icon">
+                <SiOpencv size={30} style={{ color: "white" }} />
+                <p>OpenCV</p>
+              </div>
+            </div>
+          </div>
+          <div className="skill">
+            <h3>Computer Graphics</h3>
+            <p>Tech Stack:</p>
+            <div className="techstack">
+              <div className="tech-icon">
+                <SiCplusplus size={30} style={{ color: "white" }} />
+                <p>C++</p>
+              </div>
+              <div className="tech-icon">
+                <FaSwift size={30} style={{ color: "white" }} />
+                <p>Swift</p>
+              </div>
+              <div className="tech-icon">
+                <SiOpengl size={30} style={{ color: "white" }} />
+                <p>OpenGL</p>
+              </div>
+              <div className="tech-icon">
+                <TbBrandThreejs size={30} style={{ color: "white" }} />
+                <p>ThreeJS</p>
+              </div>
+              <div className="tech-icon">
+                <p>Apple Metal</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
