311d93ac0f2fe95efe94b587b3fd03e75b1a87a1
Marc Lagoin <marclagoin@MacBook-Air-de-Marc.local>
2024-11-13 22:10:09 +0100

    ADD: Contact section / TODO: styling contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,10 +1,40 @@
 import "../styles/Contact.css";
+import { FaGithub } from "react-icons/fa";
+import { SiMalt } from "react-icons/si";
+import { FaLinkedin } from "react-icons/fa";
 
 export const Contact = () => {
   return (
     <div id="contact">
       <div className="contact-container">
         <h1>Contact</h1>
+        <div className="items-contact">
+          <h3>Contact me through: </h3>
+          <a href="https://github.com/MarcLagoin1" className="contact-icons">
+            <FaGithub
+              size={40}
+              className="icon"
+              style={{ color: "white", display: "block" }}
+            />
+            <p>Github</p>
+          </a>
+          <a href="" className="contact-icons">
+            <FaLinkedin
+              className="icon"
+              size={40}
+              style={{ color: "white", display: "block" }}
+            />
+            <p>LinkedIn</p>
+          </a>
+          <a href="" className="contact-icons">
+            <SiMalt
+              className="icon"
+              size={70}
+              style={{ color: "white", display: "block" }}
+            />
+            <p>Malt</p>
+          </a>
+        </div>
       </div>
     </div>
   );
